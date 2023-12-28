7d02a0341ef03c9276e16f69251c75ce24607d52
bakakoozin <cyrilpebre@gmail.com>
2023-12-28 22:34:38 +0100

    modif index.js

--- back/index.js
+++ back/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
-const port = 5000;
-app.use(cors());
+const app = express()
+const port = 5000
+app.use(cors())
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello World !' });
-});
+  res.send({ message: 'Hello World !' })
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
